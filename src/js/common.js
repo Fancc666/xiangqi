f63b1099afe2dfb80c892d5d378dd66d918c7db1
丁浩然 <dinghaoran@tiaozhan.com>
2018-03-02 16:36:56 +0800

    完善README.md；增加common.js中的图片的初步加载（尚未完成）

--- src/js/common.js
+++ src/js/common.js
@@ -2,7 +2,7 @@
 
 var com = com || {};
 
-com.init = function (style) {
+com.init = function(style) {
 	com.nowStyle = style || com.getCookie("style") || "style1";
 	var style = com.style[com.nowStyle];
 	com.width = style.width;             // 画布宽度
@@ -48,6 +48,100 @@ com.style = {
 }
 
 // 通过id获取元素
-com.get = function (id) {
+com.get = function(id) {
 	return document.getElementById(id);
+}
+
+window.onload = function() {
+	com.bg = new com.class.Bg();
+	com.dot = new com.class.Dot();
+	com.pane = new com.class.Pane();
+	com.pane.isShow = false;
+
+	com.childList = [com.bg, com.dot, com.pane];
+	com.mans = {}; // 棋子集合
+	com.createMans(com.initMap); // 生成棋子
+	com.bg.show();
+	com.get("btnBox").style.display = "block";
+
+	com.get("billBtn").addEventListener("click", function(e) {
+		if (confirm("是否结束对局，开始棋局研究？")) {
+			com.init();
+			com.get("chessRight").style.display = "block";
+			com.get("moveInfo").style.display = "none";
+			bill.init();
+		}
+	});
+	com.get("superPlay").addEventListener("click", function(e) {
+		if (confirm("确认开始高手级对弈？")) {
+			play.isPlay = true;
+			com.get("chessRight").style.display = "none";
+			com.get("moveInfo").style.display = "block";
+			com.get("moveInfo").innerHTML = "";
+			play.depth = 5;
+			play.init();
+		}
+	});
+	com.get("tyroPlay").addEventListener("click", function(e) {
+		if (confirm("确认开始新手级对弈？")) {
+			play.isPlay = true;
+			com.get("chessRight").style.display = "none";
+			com.get("moveInfo").style.display = "block";
+			com.get("moveInfo").innerHTML = "";
+			play.depth = 3;
+			play.init();
+		}
+	});
+
+	com.get("styleBtn").addEventListener("click", function(e) {
+		var style = com.nowStyle;
+		if (style == "style1")
+			style = "style2";
+		else if (style == "style2")
+			style = "style1";
+		com.init(style);
+		com.show();
+		play.depth = 4;
+		play.init();
+		document.cookie = "style=" + style;
+		clearInterval(timer);
+		var i = 0;
+		var timer = setInterval(function() {
+			com.show();
+			if (i++ >= 5)
+				clearInterval(timer);
+		}, 2000);
+	});
+
+	com.getData("src/js/gambit.all.js", function(data) {
+		com.gambit = data.split("");
+		AI.historyBill = com.gambit;
+	});
+	com.getData("src/js/store.js", function(data) {
+		com.store = data.split("");
+	});
+}
+
+// 载入图片
+com.loadImages = function(style) {
+	// 绘制棋盘
+	com.bgImg = new Image();
+	com.bgImg.src = "src/img/" + style + "/bg.png";
+
+	// 提示点
+	com.dotImg = new Image();
+	com.dotImg.src = "src/img/" + style + "/dot.png";
+
+	// 棋子
+	for (var i in com.args) {
+		com[i] = {};
+		com[i].img = new Image();
+		com[i].img.src = "src/img/" + style + "/" + com.args[i].img + ".png";
+	}
+
+	// 棋子外框
+	com.paneImg = new Image();
+	com.paneImg.src = "src/img/" + style + "/r_box.png";
+
+	document.getElementsByTagName("body")[0].style.background = "url(src/img/" + style + "/bg.jpg)";
 }
