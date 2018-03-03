81351a7e355bb9fb6ddd81d07c4a7fb7d8c47d3c
丁浩然 <dinghaoran@tiaozhan.com>
2018-03-03 11:29:34 +0800

    实现棋子将，炮，卒的可行着法的遍历；完成基本所有棋子遍历函数

--- src/js/common.js
+++ src/js/common.js
@@ -497,3 +497,152 @@ com.bylaw.s = function(x, y, map, my) {
 	}
 	return d;
 }
+
+// 将
+com.bylaw.j = function(x, y, map, my) {
+	var d = [];
+	var isNull = (function(y1, y2) {
+		var y1 = com.mans["j0"].y;
+		var x1 = com.mans["J0"].x;
+		var y2 = com.mans["J0"].y;
+		for (var i = y1-1; i > y2; i--) {
+			if (map[i][x1])
+				return false;
+		}
+		return true;
+	})();
+
+	if (my === 1) { // 红方
+		// 下
+		if (y+1 <= 9 && (!com.mans[map[y+1][x]] || com.mans[map[y+1][x]].my != my))
+			d.push([x, y+1]);
+		// 上
+		if (y-1 >= 7 && (!com.mans[map[y-1][x]] || com.mans[map[y-1][x]].my != my))
+			d.push([x, y-1]);
+		// 老将对老将的情况
+		if (com.mans["j0"].x == com.mans["J0"].x && isNull)
+			d.push([com.mans["J0"].x, com.mans["J0"].y]);
+	} else {
+		// 下
+		if (y+1 <= 2 && (!com.mans[map[y+1][x]] || com.mans[map[y+1][x]].my != my))
+			d.push([x, y+1]);
+		// 上
+		if (y-1 >= 0 && (!com.mans[map[y-1][x]] || com.mans[map[y-1][x]].my != my))
+			d.push([x, y-1]);
+		// 老将对老将的情况
+		if (com.mans["j0"].x == com.mans["J0"].x && isNull)
+			d.push([com.mans["j0"].x, com.mans["j0"].y]);
+	}
+	// 右
+	if (x+1 <= 5 && (!com.mans[map[y][x+1]] || com.mans[map[y][x+1]].my != my))
+		d.push([x+1, y]);
+	// 左
+	if (x-1 >= 3 && (!com.mans[map[y][x-1]] || com.mans[map[y][x-1]].my != my))
+		d.push([x-1, y]);
+	return d;
+}
+
+// 炮
+com.bylaw.p = function(x, y, map, my) {
+	var d = [];
+
+	// 左侧检索
+	var n = 0;
+	for (var i = x-1; i >= 0; i--) {
+		if (map[y][i]) {
+			if (n == 0) {
+				n++;
+				continue;
+			} else {
+				if (com.mans[map[y][i]].my != my)
+					d.push([i, y]);
+				break;
+			}
+		} else {
+			if (n == 0)
+				d.push([i, y]);
+		}
+	}
+	// 右侧检索
+	var n = 0;
+	for (var i = x+1; i <= 8; i++) {
+		if (map[y][i]) {
+			if (n == 0) {
+				n++;
+				continue;
+			} else {
+				if (com.mans[map[y][i]].my != my)
+					d.push([i, y]);
+				break;
+			}
+		} else {
+			if (n == 0)
+				d.push([i, y]);
+		}
+	}
+	// 上方检索
+	var n = 0;
+	for (var i = y-1; i >= 0; i--) {
+		if (map[i][x]) {
+			if (n == 0) {
+				n++;
+				continue;
+			} else {
+				if (com.mans[map[i][x]].my != my)
+					d.push([x, i]);
+				break;
+			}
+		} else {
+			if (n == 0)
+				d.push([x, i]);
+		}
+	}
+	// 下方检索
+	var n = 0;
+	for (var i = y+1; i <= 9; i++) {
+		if (map[i][x]) {
+			if (n == 0) {
+				n++;
+				continue;
+			} else {
+				if (com.mans[map[i][x]].my != my)
+					d.push([x, i]);
+				break;
+			}
+		} else {
+			if (n == 0)
+				d.push([x, i]);
+		}
+	}
+	
+	return d;
+}
+
+// 卒
+com.bylaw.z = function(x, y, map, my) {
+	var d = [];
+
+	if (my === 1) { // 红方
+		// 上
+		if (y-1 >= 0 && (!com.mans[map[y-1][x]] || com.mans[map[y-1][x]].my != my))
+			d.push([x, y-1]);
+		// 右
+		if (x+1 <= 8 && y <= 4 && (!com.mans[map[y][x+1]] || com.mans[map[y][x+1]].my != my))
+			d.push([x+1, y]);
+		// 左
+		if (x-1 >= 0 && y <= 4 && (!com.mans[map[y][x-1]] || com.mans[map[y][x-1]].my != my))
+			d.push([x-1, y]);
+	} else {
+		// 下
+		if (y+1 <= 9 && (!com.mans[map[y+1][x]] || com.mans[map[y+1][x]].my != my))
+			d.push([x, y+1]);
+		// 右
+		if (x+1 <= 8 && y >= 6 && (!com.mans[map[y][x+1]] || com.mans[map[y][x+1]].my != my))
+			d.push([x+1, y]);
+		// 左
+		if (x-1 >= 0 && y >= 6 && (!com.mans[map[y][x-1]] || com.mans[map[y][x-1]].my != my))
+			d.push([x-1, y]);
+	}
+
+	return d;
+}
