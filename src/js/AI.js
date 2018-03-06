4ecf7391fbc05fbd56e7fc59a7fa476c8a5879e7
丁浩然 <dinghaoran@tiaozhan.com>
2018-03-06 09:29:38 +0800

    实现棋谱分析，结合迭代加深搜索，以及评估价值函数；基本完成AI部分（尚未调试）

--- src/js/AI.js
+++ src/js/AI.js
@@ -51,3 +51,151 @@ AI.init = function(pace) {
 		return false;
 	}
 }
+
+// 迭代加深搜索着法
+AI.iterativeSearch = function(map, my) {
+	var timeOut = 100;
+	var initDepth = 1;
+	var maxDepth = 8;
+	AI.treeDepth = 0;
+	var initTime = new Date().getTime();
+	var val = {};
+	for (var i = initDepth; i <= maxDepth; i++) {
+		var nowTime = new Date().getTime();
+		AI.treeDepth = i;
+		AI.autoDepth = i;
+		var val = AI.getAlphaBeta(-99999, 99999, AI.treeDepth, map, my);
+		if (nowTime - initTime > timeOut) {
+			return val;
+		}
+	}
+	return false;
+}
+
+// 取得棋盘上所有棋子
+AI.getMapAllMan = function(map, my) {
+	var mans = [];
+	for (var i = 0; i < map.length; i++) {
+		for (var n = 0; n < map[0].length; n++) {
+			var key = map[i][n];
+			if (key && play.mans[key].my == my) {
+				play.mans[key].x = n;
+				play.mans[key].y = i;
+				mans.push(play.mans[key]);
+			}
+		}
+	}
+	return mans;
+}
+
+// 取得棋谱所有己方棋子的着法
+AI.getMoves = function(map, my) {
+	var manArr = AI.getMapAllMan(map, my);
+	var moves = [];
+	var foul = play.isFoul;
+	for (var i = 0; i < manArr.length; i++) {
+		var man = manArr[i];
+		var val = man.bl(map);
+
+		for (var n = 0; n < val.length; n++) {
+			var x = man.x;
+			var y = man.y;
+			var newX = val[n][0];
+			var newY = val[n][1];
+			// 如果不是长将着法
+			if (foul[0] != x || foul[1] != y || foul[2] != newX || foul[3] != newY) {
+				moves.push([x, y, newX, newY, man.key]);
+			}
+		}
+	}
+	return moves;
+}
+
+AI.getAlphaBeta = function(A, B, depth, map, my) {
+	if (depth == 0) {
+		return {"value": AI.evaluate(map, my)}; // 局面评价函数
+	}
+	var moves = AI.getMoves(map, my); // 生成全部走法
+	// 这里排序以后会增加效率
+
+	for (var i = 0; i < moves.length; i++) {
+		// 走这个走法
+		var move = moves[i];
+		var key = move[4];
+		var oldX = move[0];
+		var oldY = move[1];
+		var newX = move[2];
+		var newY = move[3];
+		var clearKey = map[newX][newY] || "";
+
+		map[newY][newX] = key;
+		delete map[oldY][oldX];
+		play.mans[key].x = newX;
+		play.mans[key].y = newY;
+
+		if (clearKey == "j0" || clearKey == "J0") {
+			// 被吃老将，撤销这个走法
+			play.mans[key].x = oldX;
+			play.mans[key].y = oldY;
+			map[oldY][oldX] = key;
+			delete map[newY][newX];
+			if (clearKey) {
+				map[newY][newX] = clearKey;
+			}
+
+			return {"key": key, "x": newX, "y": newY, "value": 8888};
+		} else {
+			var val = -AI.getAlphaBeta(-B, -A, depth - 1, map, -my).value;
+
+			// 撤销这个走法
+			play.mans[key].x = oldX;
+			play.mans[key].y = oldY;
+			map[oldY][oldX] = key;
+			delete map[newY][newX];
+			if (clearKey) {
+				map[newY][newX] = clearKey;
+			}
+			if (val >= B) {
+				return {"key": key, "x": newX, "y": newY, "value": B};
+			}
+			if (val > A) {
+				A = val; // 设置最佳走法
+				if (AI.treeDepth == depth)
+					var rootKey = {"key": key, "x": newX, "y": newY, "value": A};
+			}
+		}
+	}
+
+	if (AI.treeDepth == depth) {
+		// 已经递归回根了
+		if (!rootKey) {
+			// AI没有最佳走法，说明AI被将死了，返回false
+			return false;
+		} else {
+			// 这个就是最佳走法
+			return rootKey;
+		}
+	}
+	return {"key": key, "x": newX, "y": newY, "value": A};
+}
+
+// 将着法记录到历史表
+AI.setHistoryTable = function(txtMap, depth, value, my) {
+	AI.setHistoryTable.length++;
+	AI.historyTable[txtMap] = {"depth": depth, "value": value};
+}
+
+// 评估棋局，取得棋盘双方棋子价值差
+AI.evaluate = function(map, my) {
+	var val = 0;
+	for (var i = 0; i < map.length; i++) {
+		for (var n = 0; n < map[i].length; n++) {
+			var key = map[i][n];
+			if (key) {
+				val += play.mans[key].value[i][n] * play.mans[key].my;
+			}
+		}
+	}
+	AI.number++;
+	return val * my;
+}
